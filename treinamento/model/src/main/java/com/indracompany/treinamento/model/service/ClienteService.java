3f4ab4bc56d538b68f10f7405cf86a882b1030ab
Guilherme Dantas <guilherme.extreme10@gmail.com>
2022-03-19 18:51:27 -0300

    Busca por nome
    Criação da busca filtrada por nome

---
+++ treinamento/model/src/main/java/com/indracompany/treinamento/model/service/ClienteService.java
@@ -0,0 +1,40 @@
+package com.indracompany.treinamento.model.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.indracompany.treinamento.exception.AplicacaoException;
+import com.indracompany.treinamento.exception.ExceptionValidacoes;
+import com.indracompany.treinamento.model.dto.ClienteDTO;
+import com.indracompany.treinamento.model.entity.Cliente;
+import com.indracompany.treinamento.model.repository.ClienteRepository;
+
+@Service
+public class ClienteService extends GenericCrudService<Cliente, Long, ClienteRepository>{
+
+	  public List<ClienteDTO> buscarClientePorNome(String nome) {
+		  boolean nomeValido = nome != null ;
+		  
+		  if (!nomeValido) {
+			  throw new AplicacaoException(ExceptionValidacoes.ERRO_NOME_INVALIDO);
+		  }
+		  
+		  List<Cliente> clientes = repository.findByNomeStartingWith(nome);
+		  
+		  if (clientes == null || clientes.isEmpty()) {
+			  throw new AplicacaoException(ExceptionValidacoes.ALERTA_NENHUM_REGISTRO_ENCONTRADO);
+		  }
+		  
+		  
+		 
+		  List<ClienteDTO> retorno = new ArrayList<ClienteDTO>();
+		  for (Cliente c: clientes) {
+			  ClienteDTO dto = new ClienteDTO();
+			  dto.setNome(c.getNome());
+			  retorno.add(dto);
+		  }
+		  return retorno;
+	  }
+}
